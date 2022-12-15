641e68284e4201e89f6de8281d5eec59057f21b7
leovcampos <leonardoviniciuscampos@gmail.com>
2022-12-15 17:38:53 -0300

    req09

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,11 +1,12 @@
 db.produtos.find(
     {
-        valoresNutricionais: {
-            $elemMatch: {
-                tipo: 'calorias',
-                quantidade: { $lt: 500 },
-            }
-        }
+      valoresNutricionais: {
+        $elemMatch: {
+          tipo: "calorias",
+          quantidade: { $lt: 500 },
+        },
+      },
     },
     { _id: false, nome: 1 },
-);
+  );
+
