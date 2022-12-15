6b05492e26d0af1f25a99240e2104fa90fc8cf15
leovcampos <leonardoviniciuscampos@gmail.com>
2022-12-15 17:48:39 -0300

    lint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,10 +2,10 @@ db.produtos.find(
   {
     valoresNutricionais: {
       $elemMatch: {
-        tipo: 'proteínas',
-        percentual: { $gte: 30, $lte: 40 }
-      }
-    }
+        tipo: "proteínas",
+        percentual: { $gte: 30, $lte: 40 },
+      },
+    },
   },
   { _id: false, nome: 1 },
 );
