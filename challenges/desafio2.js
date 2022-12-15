a5c2d2dd6f342ed3906a4bcfdd8e73ed8690225d
leovcampos <leonardoviniciuscampos@gmail.com>
2022-12-15 15:21:07 -0300

    req02 lint

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,8 +1,1 @@
-db.produtos.find(
-    {},
-    {
-        _id: false,
-        nome: 1,
-        vendidos: 1
-    }
-).sort({ vendidos: 1 });
+db.produtos.find({}, { _id: false, nome: 1, vendidos: 1 }).sort({ vendidos: 1 });
