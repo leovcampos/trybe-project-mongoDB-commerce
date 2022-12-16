c35dbd3034ae886b38f3c84be8e06c7da5ae2dd0
leovcampos <leonardoviniciuscampos@gmail.com>
2022-12-16 14:51:28 -0300

    req16

---
+++ challenges/desafio16.js
@@ -0,0 +1,11 @@
+db.produtos.updateOne(
+    { nome: "Big Mac" },
+    {
+        $set: { ultimaModificacao: new Date() },
+    },
+);
+
+db.produtos.find(
+    { ultimaModificacao: { $exists: true } },
+    { _id: false, nome: 1 },
+);
